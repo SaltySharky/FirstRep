98cfa52822b298aa4ec27a78add04cfb9094eecc
kavon-naziri <kavon.naziri@gmail.com>
2024-11-20 21:00:18 -0800

    profile page

---
+++ frontend/src/components/ProfilePage.tsx
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+import { useAuth } from "../contexts/AuthContext";
+import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css'
+
+type ValuePiece = Date | null;
+
+type Value = ValuePiece | [ValuePiece, ValuePiece];
+
+const ProfilePage = () => {
+    const { currentUser } = useAuth();
+    const [value, onChange] = useState<Value>(new Date());
+    return (
+        <>
+            <text>John Doe</text>
+            <text>Workouts: 26</text>
+            <text>Streak: 5</text>
+            <Calendar onChange={onChange} value={value} />
+        </>
+    );
+};
+
+export default ProfilePage;
