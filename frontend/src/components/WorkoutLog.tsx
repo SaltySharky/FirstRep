65d71f03d22c9170ac6195ab861fb885cd8f0fdf
kavon-naziri <kavon.naziri@gmail.com>
2024-12-03 16:39:30 -0800

    state variable added to workout logger

--- frontend/src/components/WorkoutLog.tsx
+++ frontend/src/components/WorkoutLog.tsx
@@ -1,14 +1,46 @@
 import Navbar from "./navbar";
+import React, {useState} from "react";
 
 const WorkoutLog = () => {
+    const [workoutName, setWorkoutName] = useState('');
+    const [workoutType, setWorkoutType] = useState('');
+    const [workoutDuration, setWorkoutDuration] = useState('');
+    
     return (
         <>
             <Navbar />
             <form>
-                <input placeholder="Workout Name" />
-                <input placeholder="Workout Type" />
-                <input placeholder="Workout Duration" />
-                <button>Submit</button>
+                <div>
+                    <input 
+                        placeholder="Workout Name"
+                        value={workoutName}
+                        onChange={(e) => setWorkoutName(e.target.value)}
+                        className="w-full px-4 py-3 text-sm border border-gray-300 rounded-lg focus:outline-none focus:border-blue-500"
+                    />
+                </div>
+                <div>
+                    <input 
+                        placeholder="Workout Type"
+                        value={workoutType}
+                        onChange={(e) => setWorkoutType(e.target.value)}
+                        className="w-full px-4 py-3 text-sm border border-gray-300 rounded-lg focus:outline-none focus:border-blue-500"
+                    />
+                </div>
+                <div>
+                    <input 
+                        placeholder="Workout Duration" 
+                        value={workoutDuration}
+                        onChange={(e) => setWorkoutDuration(e.target.value)}
+                        className="w-full px-4 py-3 text-sm border border-gray-300 rounded-lg focus:outline-none focus:border-blue-500"
+                    />
+                </div>
+                <div>
+                    <button
+                    >
+                        Submit
+                    </button>
+                </div>
+                
             </form>
         </>
         
